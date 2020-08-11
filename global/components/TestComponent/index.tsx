db5936d393277a76d22cfe4f3b42982c2f9ece97
MaxLeduc <leduc.maximep@gmail.com>
2020-08-11 12:24:44 -0400

    absolute paths and shared folder (#5)

---
+++ global/components/TestComponent/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export const TestComponent = () => {
+  return (
+    <View>
+      <Text>This is a test.</Text>
+    </View>
+  );
+};
